9c1dd799e7449e560498566d16ba3d9e634f928c
BrenoMDR <79334003+BrenoMDR@users.noreply.github.com>
2024-06-23 22:35:53 -0300

    V 3.0 Ajuste estrutura de projetos

---
+++ src/js/menuLateral.js
@@ -0,0 +1,41 @@
+var script = document.createElement('script');
+script.type = "text/javascript";
+script.src = "https://cdn.jsdelivr.net/npm/sweetalert2@11";
+document.body.appendChild(script);
+
+var menuItem = document.querySelectorAll('.item-menu');
+    var btnExpandir = document.querySelector('#btnExpandir');
+    var menuLateral = document.querySelector('.menu-lateral');
+    var logoutLink = document.querySelector("#logoutLink");
+
+menuItem.forEach((item) =>{
+    item.addEventListener('click', selectLink);
+})
+
+btnExpandir.addEventListener('click', function(){
+    menuLateral.classList.toggle('expandir');
+});
+
+logoutLink.addEventListener('click', function(){
+    Swal.fire({
+        text: "Tem certeza que deseja sair?",
+        icon: "question",
+        title: "Logout",
+        showDenyButton: true,
+        confirmButtonText: "Sair",
+        denyButtonText: "Cancelar",
+        allowOutsideClick: false
+    }).then((result) => {
+        if (result.isConfirmed) {
+            location.href='../login/inicio.html';
+        }
+    });
+});
+
+function selectLink(){
+    menuItem.forEach((item) =>{
+        item.classList.remove('ativo');
+    });
+
+    this.classList.add('ativo');
+}
